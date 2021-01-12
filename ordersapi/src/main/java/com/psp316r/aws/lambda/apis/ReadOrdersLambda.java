9f0dea2b2542de55177ac61f039acd8aa389dd7c
psp316r <preetham.p1997@gmail.com>
2021-01-12 23:54:14 +0530

    DynamoDB added

---
+++ ordersapi/src/main/java/com/psp316r/aws/lambda/apis/ReadOrdersLambda.java
@@ -0,0 +1,31 @@
+package com.psp316r.aws.lambda.apis;
+
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClientBuilder;
+import com.amazonaws.services.dynamodbv2.model.ScanRequest;
+import com.amazonaws.services.dynamodbv2.model.ScanResult;
+import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyRequestEvent;
+import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyResponseEvent;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.psp316r.aws.lambda.apis.dto.Order;
+
+public class ReadOrdersLambda {
+	public APIGatewayProxyResponseEvent getOrders(APIGatewayProxyRequestEvent request) throws JsonMappingException, JsonProcessingException 
+	{
+		 ObjectMapper objectMapper = new ObjectMapper();		 
+		 AmazonDynamoDB dynamoDB = AmazonDynamoDBClientBuilder.defaultClient();
+		 ScanResult scanResult = dynamoDB.scan(new ScanRequest().withTableName(System.getenv("ORDERS_TABLE")));
+		 List <Order> orders = scanResult.getItems().stream().map(item->new Order(Integer.parseInt(item.get("id").getN()),
+				 item.get("itemName").getS(),Integer.parseInt(item.get("quantity").getN())))
+				 .collect(Collectors.toList());
+		 String jsonOutput = objectMapper.writeValueAsString(orders);
+		 return new APIGatewayProxyResponseEvent().withStatusCode(200).withBody(jsonOutput);
+	}
+
+}
---
+++ ordersapi/src/main/java/com/psp316r/aws/lambda/apis/dto/Order.java
@@ -0,0 +1,22 @@
+package com.psp316r.aws.lambda.apis.dto;
+
+public class Order {
+	
+	public int id;
+	public String itemName;
+	public int quantity;
+	
+	public Order()
+	{
+		
+	}
+	
+	public Order(int id, String itemName, int quantity)
+	{
+		this.id = id;
+		this.itemName =  itemName;
+		this.quantity = quantity;
+		
+	}
+
+}
